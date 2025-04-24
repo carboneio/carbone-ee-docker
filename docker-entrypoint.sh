48774711430704df44e5f4c91f7fe1c477a1a5a2
Guillaume Paquereau <guillaume@carbone.io>
2025-04-24 11:28:36 +0200

    Disable Chrome is SYS_ADMIN is not set

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+## For Chrome execution, we need first to test if sys_admin cap is enabled
+setpriv -d | grep sys_admin > /dev/null || unset CARBONE_EE_CHROMEPATH
+
+if [ -z "$CARBONE_EE_CHROMEPATH" ]; then
+    echo "---------- CHROME SUPPORT IS DISABLED -----------"
+    echo "Running Chrome in a Docker container requires"
+    echo "SYS_ADMIN capabilities. "
+    echo "To enable it, use the --cap-add SYS_ADMIN option."
+    echo "-------------------------------------------------"
+fi
+
 exec ./carbone-ee-linux $@
