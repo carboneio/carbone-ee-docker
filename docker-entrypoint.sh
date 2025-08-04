f37157e8599beaeac91fe94c5619140b6b3c438f
Guillaume Paquereau <guillaume@carbone.io>
2025-08-04 20:01:40 +0200

    Fix CARBONE_EE_WORKDIR usage

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,24 +2,26 @@
 
 CARBONE_USE_S3_PLUGIN=${CARBONE_USE_S3_PLUGIN:-true}
 
+CARBONE_EE_WORKDIR=${CARBONE_EE_WORKDIR:/app}
+
 CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
 if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
     touch $CONTAINER_ALREADY_STARTED
     if [ "$CARBONE_USE_AZURE_PLUGIN" = true ]; then
         echo "Configuring Carbone with Azure plugin"
-        cp -r /app/plugin-azure/node_modules /app/plugin/
-        cp -r /app/plugin-azure/*.js /app/plugin/
+        cp -r ${CARBONE_EE_WORKDIR}/plugin-azure/node_modules ${CARBONE_EE_WORKDIR}/plugin/
+        cp -r ${CARBONE_EE_WORKDIR}/plugin-azure/*.js ${CARBONE_EE_WORKDIR}/plugin/
     elif [ "$CARBONE_USE_S3_PLUGIN" = true ]; then
         echo "Configuring Carbone with S3 plugin"
-        cp -r /app/plugin-s3/node_modules /app/plugin/
-        cp -r /app/plugin-s3/*.js /app/plugin/
+        cp -r ${CARBONE_EE_WORKDIR}/plugin-s3/node_modules ${CARBONE_EE_WORKDIR}/plugin/
+        cp -r ${CARBONE_EE_WORKDIR}/plugin-s3/*.js ${CARBONE_EE_WORKDIR}/plugin/
     fi
 fi
 
 ## Set public key (for V5 compatibility)
 if [ -n "$CARBONE_AUTHENTICATION_PUBLIC_KEY" ]; then
-    mkdir /app/config
-    echo -e $CARBONE_AUTHENTICATION_PUBLIC_KEY > /app/config/key.pub
+    mkdir ${CARBONE_EE_WORKDIR}/config
+    echo -e $CARBONE_AUTHENTICATION_PUBLIC_KEY > ${CARBONE_EE_WORKDIR}/config/key.pub
 fi
 
 exec ./carbone-ee-linux $@
