5159531339a42425899f6f916c93b8dc9a612564
Guillaume Paquereau <guillaume@carbone.io>
2025-08-07 17:28:14 +0200

    Disable Sandbox if docker running without sys_admin rights

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,29 +2,33 @@
 
 CARBONE_USE_S3_PLUGIN=${CARBONE_USE_S3_PLUGIN:-true}
 
+CARBONE_EE_WORKDIR=${CARBONE_EE_WORKDIR:-/app}
+if [ $CARBONE_EE_WORKDIR != "/app" ]; then
+    mkdir ${CARBONE_EE_WORKDIR}
+fi
+
 CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
 if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
     touch $CONTAINER_ALREADY_STARTED
     if [ "$CARBONE_USE_AZURE_PLUGIN" = true ]; then
         echo "Configuring Carbone with Azure plugin"
-        cp -r /app/plugin-azure/node_modules /app/plugin/
-        cp -r /app/plugin-azure/*.js /app/plugin/
+        cp -r /app/plugin-azure/node_modules ${CARBONE_EE_WORKDIR}/plugin/
+        cp -r /app/plugin-azure/*.js ${CARBONE_EE_WORKDIR}/plugin/
     elif [ "$CARBONE_USE_S3_PLUGIN" = true ]; then
         echo "Configuring Carbone with S3 plugin"
-        cp -r /app/plugin-s3/node_modules /app/plugin/
-        cp -r /app/plugin-s3/*.js /app/plugin/
+        cp -r /app/plugin-s3/node_modules ${CARBONE_EE_WORKDIR}/plugin/
+        cp -r /app/plugin-s3/*.js ${CARBONE_EE_WORKDIR}/plugin/
     fi
 fi
 
 ## For Chrome execution, we need first to test if sys_admin cap is enabled
-setpriv -d | grep sys_admin > /dev/null || unset CARBONE_EE_CHROMEPATH
-
-if [ -z "$CARBONE_EE_CHROMEPATH" ]; then
-    echo "---------- CHROME SUPPORT IS DISABLED -----------"
-    echo "Running Chrome in a Docker container requires"
-    echo "SYS_ADMIN capabilities. "
-    echo "To enable it, use the --cap-add SYS_ADMIN option."
-    echo "-------------------------------------------------"
+## if disable, we force no-sandbox mode
+SYS_ADMIN=`setpriv -d | grep sys_admin`
+if [ "$SYS_ADMIN" = "" ]; then
+    CARBONE_CHROME_FLAGS="--no-sandbox"
+    echo "Running Chrome without sandbox"
+else
+    echo "Running Chrome with sandbox"
 fi
 
 exec ./carbone-ee-linux $@
